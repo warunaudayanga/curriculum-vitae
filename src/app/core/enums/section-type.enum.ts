090d0c2505b875428846cc1a1b934e240414101b
Waruna Udayanga <warunaudayangalakruwan@gmail.com>
2023-02-12 22:34:50 +0530

    Nice to have features

--- src/app/core/enums/section-type.enum.ts
+++ src/app/core/enums/section-type.enum.ts
@@ -2,7 +2,6 @@ export enum SectionType {
     PARAGRAPH = "Paragraph",
     LIST = "List",
     LIST_WITH_PARAGRAPH = "List With Paragraph",
-    NAME_VALUE_LIST_IN_LIST = "Name - Value List in List",
     NAME_VALUE_LIST = "Name - Value List",
     COLUMNS = "Columns",
     SIGNATURE = "Signature",
